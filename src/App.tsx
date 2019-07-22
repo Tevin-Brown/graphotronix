2710812a468270282267c766d20419c6355f0a37
Tevin Brown <tevinbrown@WTA-MBP579.local>
2019-07-22 15:48:41 -0400

    trying a hybrid approach

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PieChart from './lib/PieChart/PieChart';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <PieChart/>
+    </div>
+  );
+}
+
+export default App;
